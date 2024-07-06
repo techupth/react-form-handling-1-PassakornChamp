258bf7ca1652b6a144960e1b21fb9bb610a1cf51
Passakorn Chantarapakorn <passakornchantarapakorn@Passakorns-MacBook-Air.local>
2024-07-06 16:52:39 +0700

    Submit : add input word handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [words, setWords] = useState("Hello World");
+  const [inputText, setInputText] = useState("");
+
+  function handleInputChange(e) {
+    setInputText(e.target.value);
+  }
+
+  function handleClick() {
+    setWords(inputText);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{words}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleClick}>Update text</button>
       </div>
     </div>
   );
